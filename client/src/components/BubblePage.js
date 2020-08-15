3514c5a46f9980c7e44013a9037858d7fdcadada
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-08-15 12:54:30 -0400

    MVP

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+// import axios from "axios";
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+	const [colorList, setColorList] = useState([]);
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+	useEffect(() => {
+		async function getColors() {
+			try {
+				const response = await axiosWithAuth().get('/colors');
+				setColorList(response.data);
+			} catch (error) {
+				alert(`Something Went Wrong => ${error}`);
+			}
+		}
+		getColors();
+	}, []);
+
+	return (
+		<>
+			<ColorList colors={colorList} updateColors={setColorList} />
+			<Bubbles colors={colorList} />
+		</>
+	);
 };
 
 export default BubblePage;
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,89 +1,111 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+// import axios from 'axios';
+import axiosWithAuth from '../utils/axiosWithAuth';
+// import { useParams } from 'react-router-dom';
 
 const initialColor = {
-  color: "",
-  code: { hex: "" }
+	color: '',
+	code: { hex: '' },
+	id: '',
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
+	console.log(colors);
+	const [editing, setEditing] = useState(false);
+	const [colorToEdit, setColorToEdit] = useState(initialColor);
+	// const { id } = useParams();
 
-  const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
+	const editColor = (color) => {
+		setEditing(true);
+		setColorToEdit(color);
+	};
 
-  const saveEdit = e => {
-    e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-  };
+	const saveEdit = async (e) => {
+		try {
+			e.preventDefault();
+			const response = await axiosWithAuth().put(
+				`/colors/${colorToEdit.id}`,
+				colorToEdit
+			);
+			const filteredList = colors.filter(
+				(color) => color.id !== colorToEdit.id
+			);
+			updateColors([...filteredList, response.data]);
+			console.log(response);
+		} catch (error) {
+			alert(`Something went wrong => ${error}`);
+		}
+	};
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
-  };
+	const deleteColor = async (color) => {
+		try {
+			const response = await axiosWithAuth().delete(`/colors/${color.id}`);
+			const filteredList = colors.filter((color) => color.id !== response.data);
+			updateColors([...filteredList]);
+		} catch (error) {
+			alert(`Something went wrong => ${error}`);
+		}
+	};
 
-  return (
-    <div className="colors-wrap">
-      <p>colors</p>
-      <ul>
-        {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
-              {color.color}
-            </span>
-            <div
-              className="color-box"
-              style={{ backgroundColor: color.code.hex }}
-            />
-          </li>
-        ))}
-      </ul>
-      {editing && (
-        <form onSubmit={saveEdit}>
-          <legend>edit color</legend>
-          <label>
-            color name:
-            <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
-            />
-          </label>
-          <label>
-            hex code:
-            <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
-          </label>
-          <div className="button-row">
-            <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
-          </div>
-        </form>
-      )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
-    </div>
-  );
+	return (
+		<div className='colors-wrap'>
+			<p>colors</p>
+			<ul>
+				{colors.map((color) => (
+					<li key={color.color} onClick={() => editColor(color)}>
+						<span>
+							<span
+								className='delete'
+								onClick={(e) => {
+									e.stopPropagation();
+									deleteColor(color);
+								}}
+							>
+								x
+							</span>{' '}
+							{color.color}
+						</span>
+						<div
+							className='color-box'
+							style={{ backgroundColor: color.code.hex }}
+						/>
+					</li>
+				))}
+			</ul>
+			{editing && (
+				<form onSubmit={saveEdit}>
+					<legend>edit color</legend>
+					<label>
+						color name:
+						<input
+							onChange={(e) =>
+								setColorToEdit({ ...colorToEdit, color: e.target.value })
+							}
+							value={colorToEdit.color}
+						/>
+					</label>
+					<label>
+						hex code:
+						<input
+							onChange={(e) =>
+								setColorToEdit({
+									...colorToEdit,
+									code: { hex: e.target.value },
+								})
+							}
+							value={colorToEdit.code.hex}
+						/>
+					</label>
+					<div className='button-row'>
+						<button type='submit'>save</button>
+						<button onClick={() => setEditing(false)}>cancel</button>
+					</div>
+				</form>
+			)}
+			<div className='spacer' />
+			{/* stretch - build another form here to add a color */}
+		</div>
+	);
 };
 
 export default ColorList;
